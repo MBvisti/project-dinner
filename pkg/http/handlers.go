8bc7bfc3edab80bc5a134ffc9482e974f8f54e10
Morten Vistisen <mv@legalmonster.com>
2020-10-25 11:07:01 +0100

    moved handlers and routes in to rest package

---
+++ pkg/http/handlers.go
@@ -0,0 +1,161 @@
+package http
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	service "project-dinner/pkg/services"
+)
+
+type handlerResponse struct {
+	Status string      `json:"status"`
+	Data   interface{} `json:"data"`
+}
+
+// APIStatus returns the status of the api
+func APIStatus() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Content-Type", "application/json")
+
+		c.JSON(http.StatusOK, handlerResponse{Status: "success", Data: "project dinner api running smoothly"})
+	}
+}
+
+// // StopCronJob stops cron object running on the server struct
+// func (s *Server) StopCronJob() gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		c.Header("Content-Type", "application/json")
+
+// 		s.cron.Stop()
+
+// 		c.JSON(http.StatusOK, handlerResponse{Status: "success", Data: "the cron job is stopped"})
+// 	}
+// }
+
+// // AllRecipes ...
+// type AllRecipes struct {
+// 	Recipes []repository.Recipe `json:"recipes"`
+// }
+
+// // UserRecipe ... TODO: move this in to a user service at one point
+// type UserRecipe struct {
+// 	UserName string
+// 	Recipes  []repository.EmailRecipe
+// }
+
+// SendMails send out recipe emails
+func SendMails(u service.EmailService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		//mailTemplate, err := template.ParseFiles("./template/daily_recipe_email.html")
+		//if err != nil {
+		//	c.JSON(http.StatusInternalServerError, handlerResponse{Status: "failure", Data: err.Error()})
+		//	return
+		//}
+		//
+		//var emailList []*gomail.Message
+		//
+		//userList, err := u.
+		//	s.storage.User.GetEmailList()
+		//
+		//if err != nil {
+		//	c.JSON(http.StatusInternalServerError, handlerResponse{Status: "failure", Data: err.Error()})
+		//	return
+		//}
+		//
+		//recipes, err := s.storage.Recipe.GetRandomRecipes()
+		//
+		//if err != nil {
+		//	c.JSON(http.StatusInternalServerError, handlerResponse{Status: "failure", Data: err.Error()})
+		//	return
+		//}
+		//
+		//for _, user := range userList {
+		//	usrRecipe := UserRecipe{
+		//		UserName: user.Name,
+		//		Recipes:  recipes,
+		//	}
+		//
+		//	var t bytes.Buffer
+		//	err = mailTemplate.Execute(&t, usrRecipe)
+		//
+		//	mail := gomail.NewMessage()
+		//	mail.SetAddressHeader("From", "noreply@mbvistisen.dk", "Morten's recipe service")
+		//	mail.SetHeader("To", user.Email)
+		//	mail.SetHeader("Subject", "Your daily recipes are here!")
+		//	mail.SetBody("text/html", t.String())
+		//
+		//	emailList = append(emailList, mail)
+		//}
+		//
+		//err = s.mailer.DialAndSend(emailList...)
+		//
+		//if err != nil {
+		//	c.JSON(http.StatusInternalServerError, handlerResponse{Status: "failure", Data: err.Error()})
+		//	return
+		//}
+
+		c.JSON(http.StatusOK, handlerResponse{Status: "success", Data: "all emails sent"})
+
+	}
+}
+
+// CrawlSite endpint gets all links to a recipe on the clean eating couple, scrape the recipe and stores it in db
+func CrawlSite() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Content-Type", "application/json")
+
+		//pages := 10
+		//var links []string
+		//baseURL := "https://thecleaneatingcouple.com/category/recipes/lunch-dinner/page/"
+		//
+		//for page := 1; page < pages; page++ {
+		//	newLinks := s.CrawlUrls(baseURL + strconv.Itoa(page))
+		//	links = append(links, newLinks...)
+		//}
+		//
+		//var returnedData []repository.Recipe
+		//for _, link := range links {
+		//	res, err := s.Crawler(link)
+		//	if err != nil {
+		//		log.Printf("this is the crawler error: %v", err.Error())
+		//		continue
+		//	}
+		//
+		//	if res.Description == "" {
+		//		continue
+		//	}
+		//	returnedData = append(returnedData, res)
+		//	err = s.storage.Recipe.CreateScrapedRecipe(res)
+		//
+		//	if err != nil {
+		//		log.Printf("this is the storage error: %v", err.Error())
+		//		continue
+		//	}
+		//}
+
+		c.JSON(http.StatusOK, handlerResponse{Status: "success", Data: "website crawled"})
+	}
+}
+
+// SignupUser endpoint
+func SignupUser(u service.UserService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Content-Type", "application/json")
+
+		var user service.User
+
+		err := c.ShouldBindJSON(&user)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, handlerResponse{Status: "failure", Data: "user data no good"})
+			return
+		}
+
+		err = u.CreateUser(user)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, handlerResponse{Status: "failure", Data: err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, handlerResponse{Status: "success", Data: "user created"})
+	}
+}
---
+++ pkg/rest/routes.go
@@ -0,0 +1,26 @@
+package rest
+
+import (
+	service "project-dinner/pkg/services"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+// Routes set up the routes
+func Routes(uS service.UserService, eS service.EmailService) *gin.Engine {
+	r := gin.Default()
+	r.Use(cors.Default())
+
+	v1 := r.Group("/v1/api")
+	{
+		v1.GET("/status", APIStatus())
+		v1.GET("/test", GetRes(eS))
+		// v1.POST("/stop-cron", StopCronJob())
+		v1.GET("/send-mails", SendMails(eS))
+		//v1.GET("/start-scraping-procedure", CrawlSite())
+		v1.POST("/sign-up", SignupUser(uS))
+	}
+
+	return r
+}
