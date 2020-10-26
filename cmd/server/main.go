cd11eb5a6450094b7b290505d5d4a5f9abb96e95
Morten Vistisen <mv@legalmonster.com>
2020-10-26 14:44:08 +0100

    committing these changes for now - still needs clean up and updating

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
+	"net/http"
 	"os"
-	app "project-dinner/pkg"
 	"project-dinner/pkg/repository"
+
+	"project-dinner/pkg/api"
+	"project-dinner/pkg/rest"
 	"strconv"
 	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
-	"github.com/robfig/cron/v3"
 	"gopkg.in/gomail.v2"
 )
 
@@ -24,7 +27,7 @@ func main() {
 }
 
 func run() error {
-	port := os.Getenv("PORT")
+	//port := os.Getenv("PORT")
 	whatEnv := os.Getenv("WHAT_ENVIRONMENT_IS_THIS")
 	sendGridUser := os.Getenv("SEND_GRID_USER")
 	sendGridPassword := os.Getenv("SEND_GRID_API_KEY")
@@ -32,12 +35,13 @@ func run() error {
 	connectionString := os.Getenv("DATABASE_URL")
 	mailPort, err := strconv.Atoi(os.Getenv("MAIL_PORT"))
 
-	gin.SetMode(gin.ReleaseMode)
+	//gin.SetMode(gin.ReleaseMode)
 
-	if whatEnv == "development" {
-		port = "5000"
-		gin.SetMode(gin.DebugMode)
-	}
+	gin.SetMode(gin.DebugMode)
+	//if whatEnv == "development" {
+	//	port = "5000"
+	//	gin.SetMode(gin.DebugMode)
+	//}
 
 	r := gin.Default()
 	r.Use(cors.Default())
@@ -49,23 +53,29 @@ func run() error {
 	}
 
 	s := repository.NewStorage(database)
+	m := gomail.NewDialer(mailHost, mailPort, sendGridUser, sendGridPassword)
+
+	usrService := api.NewUserService(s)
+	emailService := api.NewEmailService(m, s)
+
+	router := rest.Routes(usrService, emailService)
+	log.Printf("starting server on: 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
 
 	defer database.Close()
-	s.MigrateTables()
+	//s.MigrateTables()
 
-	t, err := time.LoadLocation("Europe/Copenhagen")
+	//t, err := time.LoadLocation("Europe/Copenhagen")
 
 	if err != nil {
 		return err
 	}
 
-	c := cron.New(cron.WithLocation(t))
+	//c := cron.New(cron.WithLocation(t))
 
-	m := gomail.NewDialer(mailHost, mailPort, sendGridUser, sendGridPassword)
-
-	server := app.NewServer(s, r, c, m)
-
-	err = server.Run(":" + port)
+	//server := app.NewServer(s, r, c, m)
+	//
+	//err = server.Run(":" + port)
 
 	if err != nil {
 		return err
