f9245e5631da67250c74104c5fd2616b43657fbe
Morten Vistisen <mv@legalmonster.com>
2020-10-26 19:43:19 +0100

    Update routes.go

--- pkg/rest/routes.go
+++ pkg/rest/routes.go
@@ -8,18 +8,16 @@ import (
 )
 
 // Routes set up the routes
-func Routes(uS service.UserService, eS service.EmailService) *gin.Engine {
+func Routes(userService service.UserService, emailService service.EmailService, spiderService service.SpiderService) *gin.Engine {
 	r := gin.Default()
 	r.Use(cors.Default())
 
 	v1 := r.Group("/v1/api")
 	{
 		v1.GET("/status", APIStatus())
-		v1.GET("/test", GetRes(eS))
-		// v1.POST("/stop-cron", StopCronJob())
-		v1.GET("/send-mails", SendMails(eS))
-		//v1.GET("/start-scraping-procedure", CrawlSite())
-		v1.POST("/sign-up", SignupUser(uS))
+		v1.POST("/sign-up", SignupUser(userService))
+		v1.GET("/send-mails", SendMails(emailService))
+		v1.GET("/start-scraping-procedure", StartSpider(spiderService))
 	}
 
 	return r
