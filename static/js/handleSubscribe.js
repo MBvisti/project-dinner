c7a407c55a3c292fb79f95aabea06addd8abc374
Morten Vistisen <mv@legalmonster.com>
2020-11-03 18:58:10 +0100

    subscribe handler in good state now

--- static/js/handleSubscribe.js
+++ static/js/handleSubscribe.js
@@ -1,25 +1,21 @@
 async function Subscribe(event) {
     event.preventDefault()
     const data = new FormData(event.target)
-
     let object = {}
     data.forEach(function(value, key){
         object[key] = value;
     });
-
-    try {
-        const req = await fetch("/v1/api/sign-up", {
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            method: "post",
-            body: JSON.stringify(object),
-        })
-
-        console.log(req)
-    } catch (e) {
-        console.log(e)
-    } finally {
-        return "hello"
+    const req = await fetch("/v1/api/sign-up", {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: "post",
+        body: JSON.stringify(object),
+    })
+    if (req.status === 200) {
+        return window.location.replace("/success")
+    }
+    if (req.status === 400) {
+        return window.location.replace("/failure")
     }
 }
