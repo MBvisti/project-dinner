ab42f2f2ad3b51888de526c8df8fb159ceb4d61e
Morten Vistisen <mv@legalmonster.com>
2020-11-03 20:22:16 +0100

    updated routes in handle subscribe

--- static/js/handleSubscribe.js
+++ static/js/handleSubscribe.js
@@ -12,10 +12,12 @@ async function Subscribe(event) {
         method: "post",
         body: JSON.stringify(object),
     })
+
     if (req.status === 200) {
-        return window.location.replace("/success")
+        // TODO: will only redirect back to font page now
+        return window.location.replace("/subscribe/success")
     }
     if (req.status === 400) {
-        return window.location.replace("/failure")
+        return window.location.replace("/subscribe/failure")
     }
 }
