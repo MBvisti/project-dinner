1e9dcdec9c33b702bc97139062a4272f8bfe1be9
Morten Vistisen <mv@legalmonster.com>
2020-11-10 17:04:52 +0100

    updating user type

--- pkg/api/user.go
+++ pkg/api/user.go
@@ -2,12 +2,6 @@ package api
 
 // User ....
 type User struct {
-	Name        string `json:"name"`
-	Email       string `json:"email"`
-	RecipeType  string `json:"recipe_type"`
-	DietaryType string `json:"dietary_type"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
 }
-
-//type Recipe struct {
-//	Name string
-//}
